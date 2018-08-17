7035a27719e1c85415e6e876bc7a0fda8847c880
Andrea F. Daniele <afdaniele@ttic.edu>
2018-08-17 17:21:21 -0500

    removed docker

--- docker/assets/launch_system_monitor.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-git -C /home/software/catkin_ws/src/ros-system-monitor pull origin master
-
-source /home/software/catkin_ws/devel/setup.bash
-roslaunch /system_monitor.launch
--- docker/build-all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker build -t afdaniele/ros-system-monitor:latest ./
-docker build -t afdaniele/ros-system-monitor-standalone:latest ./standalone/
-docker build -t afdaniele/ros-system-monitor-aido:latest ./aido/
-docker build -t afdaniele/ros-system-monitor-aido-standalone:latest ./aido-standalone/
